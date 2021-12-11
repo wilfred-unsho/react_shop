16fe6e7cb12d67b15eeb69aec42e4dd6dada5c19
Wilfred <wilf.funsho@gmail.com>
2021-12-11 11:27:26 +0100

    React Shop With Cart - useState & useEffect to manage states

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import Products from "./Products";
+import Header from "./Header";
+import Footer from "./Footer";
+import React, {useEffect, useState} from "react";
+import "./App.css"
+import {Routes, Route} from 'react-router-dom'
+import Detail from "./Detail";
+import Cart from "./Cart";
+
+export default function App() {
+    const [cart, setCart] = useState(() => {
+        try{
+            return JSON.parse(localStorage.getItem("cart")) ?? []
+        }catch {
+            console.error("The cart could not be parsed into JSON")
+            return []
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cart))
+    }, [cart]);
+
+
+
+    function addToCart(id, sku) {
+        setCart((items) => {
+            const itemInCart = items.find((i) => i.sku === sku)
+            if (itemInCart) {
+                // Return new array with the matching item replaced
+                return items.map((i) => i.sku === sku ? {...i, quantity: i.quantity + 1} : i)
+            } else {
+                // Return a new array with the new item appended
+                return [...items, {id, sku, quantity: 1}]
+            }
+        })
+    }
+
+    function updateQuantity(sku, quantity) {
+        setCart((items) => {
+            return quantity === 0 ? items.filter((i) => i.sku !== sku)
+                : items.map((i) => i.sku === sku ? {...i, quantity} : i)
+        })
+    }
+
+    return (
+        <>
+            <div className="content">
+                <Header/>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<h1>Welcome to famed online shoes shop</h1>}/>
+                        <Route path="/:category" element={<Products/>}/>
+                        <Route path="/:category/:id" element={<Detail addToCart={addToCart}/>}/>
+                        <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity}/>}/>
+                    </Routes>
+                </main>
+            </div>
+            <Footer/>
+        </>
+    )
+}
---
+++ src/services/useFetch.jsx
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+
+const baseUrl = process.env.REACT_APP_API_BASE_URL;
+
+export default function useFetch(url) {
+    // Declare state variable
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+
+    useEffect(() => {
+        async function init(){
+            try{
+                const response = await fetch(baseUrl + url)
+                if(response.ok)
+                {
+                    const json = await response.json()
+                    setData(json)
+                }else{
+                    throw error
+                }
+            }catch (e) {
+                setError(e)
+            }finally {
+                setLoading(false)
+            }
+        }
+
+        init()
+
+    }, [url]);
+    return {data, error, loading}
+}
